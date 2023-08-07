e592e955816de7f0a8286c906490930b921a7b3e
adamu <69142001+Adamcy-lvts@users.noreply.github.com>
2023-08-07 07:16:41 +0100

    Update nav-desktop.blade.php

--- resources/views/components/nav-desktop.blade.php
+++ resources/views/components/nav-desktop.blade.php
@@ -5,31 +5,31 @@
                         <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                             <x-jet-nav-link
                                 class="  font-light uppercase text-white hover:text-teal-400 hover:border-teal-300 focus:text-teal-700 focus:border-teal-300"
-                                @click="triggerNavItem('#about')" href="#">
+                                @click="triggerNavItem('#about')" >
                                 {{ __('About') }}
                             </x-jet-nav-link>
 
                             <x-jet-nav-link
                                 class="  font-light uppercase text-white hover:text-teal-400 hover:border-teal-300 focus:text-teal-700 focus:border-teal-300"
-                                @click="triggerNavItem('#services')" href="#">
+                                @click="triggerNavItem('#services')" >
                                 {{ __('Services') }}
                             </x-jet-nav-link>
 
                             <x-jet-nav-link
                                 class="  font-light uppercase text-white hover:text-teal-400 hover:border-teal-300 focus:text-teal-700 focus:border-teal-300"
-                                @click="triggerNavItem('#portfolio')" href="#">
+                                @click="triggerNavItem('#portfolio')" >
                                 {{ __('Portfolio') }}
                             </x-jet-nav-link>
 
                             <x-jet-nav-link
                                 class="  font-light uppercase text-white hover:text-teal-400 hover:border-teal-300 focus:text-teal-700 focus:border-teal-300"
-                                @click="triggerNavItem('#contact')" href="#">
+                                @click="triggerNavItem('#contact')" >
                                 {{ __('Contact') }}
                             </x-jet-nav-link>
 
                             <x-jet-nav-link
                                 class=" font-light uppercase text-white hover:text-teal-400 hover:border-teal-300 focus:text-teal-700 focus:border-teal-300"
-                                @click="triggerNavItem('#blog')" href="#">
+                                @click="triggerNavItem('#blog')" >
                                 {{ __('Blog') }}
                             </x-jet-nav-link>
                         </div>
