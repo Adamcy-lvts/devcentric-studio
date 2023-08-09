96b4caccfc0f44a6d1605dd8d61a2ec51d91497d
adamu <69142001+Adamcy-lvts@users.noreply.github.com>
2023-08-09 16:29:28 +0100

    update blog

--- app/Http/Livewire/PostIndex.php
+++ app/Http/Livewire/PostIndex.php
@@ -20,20 +20,20 @@ class PostIndex extends Component
 
     public function render()
     {
-        SEOMeta::setTitle('Blog - Devcentric Studio');
+        SEOMeta::setTitle('Blog Posts');
         SEOMeta::setDescription('Explore our blog for tech-related topics and stay updated with the latest industry trends.');
         SEOMeta::setCanonical(url()->current());
 
         OpenGraph::setDescription('Explore our blog for tech-related topics and stay updated with the latest industry trends.');
-        OpenGraph::setTitle('Blog - Devcentric Studio');
+        OpenGraph::setTitle('Blog Posts');
        
 
         OpenGraph::setUrl(url()->current());
 
-        TwitterCard::setTitle('Devcentric Studio - Blog');
+        TwitterCard::setTitle('Blog Posts');
         TwitterCard::setSite('@devcentricstudio');
 
-        JsonLd::setTitle('Devcentric Studio - Blog');
+        JsonLd::setTitle('Blog Posts');
         JsonLd::setDescription('Explore the latest tech-related articles and blog posts on Devcentric Studio. Stay updated with industry trends and insights.');
         
         $posts = Post::when($this->search, function ($query) {
--- resources/views/livewire/post-index.blade.php
+++ resources/views/livewire/post-index.blade.php
@@ -2,15 +2,15 @@
     <!-- post-index.blade.php -->
 
     <div class="md:px-24 sm:px-12 lg:px-48 mx-auto py-8">
-        <a href="/" class="block text-primary font-semibold hover:underline">
-            <svg
-                class="w-8 sm:w-10 text-gray-500 hover:text-teal-600 gradient-fill"  fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70.81 62.03">
+        <a href="/" class="block text-primary ml-4 sm:ml-0 mb-2 ">
+
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-8 sm:w-10 text-teal-600 sm:text-gray-500 hover:text-teal-600 gradient-fill" fill="currentColor"
+                viewBox="0 0 495.398 495.398">
                 <path
-                    d="M58.5 62a2.3 2.3 0 0 0 2.3-2.29V35.24h6a4 4 0 0 0 2.48-7.09l-7.75-6.75V10.16a2.38 2.38 0 0 0-2.38-2.38h-4.92a2.38 2.38 0 0 0-2.38 2.38V13L38 1a4 4 0 0 0-5.22 0L1.37 28.25a4 4 0 0 0 2.61 7h6v24.5A2.3 2.3 0 0 0 12.29 62h13.54V44.21a4.56 4.56 0 0 1 4.56-4.55h10A4.56 4.56 0 0 1 45 44.21V62Z"
-                    data-name="Layer 2"  />
+                    d="m487.083 225.514-75.08-75.08v-86.73c0-15.682-12.708-28.391-28.413-28.391-15.669 0-28.377 12.709-28.377 28.391v29.941L299.31 37.74c-27.639-27.624-75.694-27.575-103.27.05L8.312 225.514c-11.082 11.104-11.082 29.071 0 40.158 11.087 11.101 29.089 11.101 40.172 0l187.71-187.729c6.115-6.083 16.893-6.083 22.976-.018l187.742 187.747a28.337 28.337 0 0 0 20.081 8.312c7.271 0 14.541-2.764 20.091-8.312 11.086-11.086 11.086-29.053-.001-40.158z" />
+                <path
+                    d="M257.561 131.836c-5.454-5.451-14.285-5.451-19.723 0L72.712 296.913a13.977 13.977 0 0 0-4.085 9.877v120.401c0 28.253 22.908 51.16 51.16 51.16h81.754v-126.61h92.299v126.61h81.755c28.251 0 51.159-22.907 51.159-51.159V306.79c0-3.713-1.465-7.271-4.085-9.877L257.561 131.836z" />
             </svg>
-         
         </a>
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 items-center mb-6 px-5 sm:px-0">
             <h1 class="text-2xl font-bold">All Posts</h1>
@@ -44,17 +44,20 @@
                             <a href="{{ route('post.show', $post->slug) }}">
                                 <h2 class="text-lg font-semibold">{{ $post->title }}</h2>
                             </a>
-                            <div class="hidden xl:block">
-                                <p class="text-gray-600 text-sm md:text-lg mt-2">{!! \Str::words($post->content, 60) !!}</p>
-                            </div>
 
-                            <div class="block lg:hidden">
-                                <p class=" text-gray-600 text-sm md:text-lg mt-2">{!! \Str::words($post->content, 20) !!}</p>
-                            </div>
+                            <p class=" text-gray-600 text-sm md:text-lg mt-2">{{ $post->description }}</p>
+
 
                         </div>
                         <div class="flex justify-between">
-                            <x-button href="{{ route('post.show', $post->slug) }}" sm outline teal label="Read More" />
+                            <div class="hidden lg:block">
+                                <x-button href="{{ route('post.show', $post->slug) }}" sm outline teal
+                                    label="Read More" />
+                            </div>
+                            <div class="block lg:hidden">
+                                <x-button href="{{ route('post.show', $post->slug) }}" xs outline teal
+                                    label="Read More" />
+                            </div>
                             <p class="text-gray-600 text-sm mt-2">
                                 Published on {{ $post->created_at->format('M d, Y') }} by {{ $post->user->name }}
                             </p>
@@ -69,7 +72,7 @@
         </div>
     </div>
 
-       
+
 
 
 </div>
