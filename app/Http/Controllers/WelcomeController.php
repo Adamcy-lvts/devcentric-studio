90a45547b392d4abef8fe9e8d79efbb2ff2fa156
adamu <69142001+Adamcy-lvts@users.noreply.github.com>
2023-08-25 00:58:41 +0100

    Update WelcomeController.php

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,7 +11,7 @@ class WelcomeController extends Controller
     public function profile() {
         
         $profile = Profile::first();
-        $posts = Post::latest()->take(3)->get();
+        $posts = Post::where('is_visible', 1)->latest()->take(3)->get();
 
         return view('welcome', [
             'profile' => $profile,
