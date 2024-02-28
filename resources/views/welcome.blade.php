8919675374c60467c3a19b170f82fa8d0a37673b
adamu <69142001+Adamcy-lvts@users.noreply.github.com>
2024-02-28 11:02:43 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -87,8 +87,8 @@
                     <div class="container relative z-30 pt-20 pb-12 sm:pt-56 sm:pb-48 lg:pt-64 lg:pb-48">
                         <div class="flex flex-col items-center justify-center lg:flex-row">
                             <div class="rounded-full border-4 border-white animate__animated animate__bounceIn">
-                                {{-- <img src="{{ asset('storage/' . $profile->user->profile_photo_path) }}"
-                                    class="h-48 rounded-full sm:h-56" alt="author" /> --}}
+                                <img src="{{ asset('storage/' . $profile->user->profile_photo_path) }}"
+                                    class="h-48 rounded-full sm:h-56" alt="author" />
 
                             </div>
                             <div class="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
