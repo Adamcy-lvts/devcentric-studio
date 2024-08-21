12a1d8f28c5488c0120556ce6c67b64b644f3fa9
Adamcy-lvts <lv4mj1@gmail.com>
2024-08-21 18:00:55 +0100

    Update config seotools

--- config/seotools.php
+++ config/seotools.php
@@ -9,9 +9,9 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'        => "Devcentric Studio", // set false to total remove
-            'titleBefore'  => "Devcentric Studio | Home", // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
-            'description'  => "Devcentric Studio is a leading provider of web development, web design, maintenance and support, graphic design, digital marketing, IT consulting, technology consulting, and digital transformation services. We specialize in delivering high-quality solutions tailored to meet the unique needs of our clients. With our expertise in HTML, CSS, JavaScript, PHP, and Laravel, we create custom web applications, responsive web designs, and e-commerce solutions. Our team of experienced professionals ensures that your website is visually appealing, user-friendly, and optimized for search engines. Partner with us to transform your digital presence and achieve your business goals.", // set false to total remove
+            'title'        => "Devcentric", // set false to total remove
+            'titleBefore'  => "Devcentric | Home", // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
+            'description'  => "Devcentric stands at the forefront of digital innovation, crafting cutting-edge software solutions that empower organizations across diverse sectors. Our expertise spans custom software development, web and mobile applications, cloud solutions, AI integration, and data analytics. From startups to enterprise-level organizations, we tailor our approach to meet the unique needs of each client, driving efficiency, enhancing user experiences, and fostering sustainable growth. Our team of passionate technologists and problem-solvers is committed to delivering innovative solutions that not only meet but exceed our clients' expectations, positioning them at the cutting edge of their industries.", // set false to total remove
             'separator'    => ' - ',
             'keywords'     => [
                                 'IT',
@@ -67,8 +67,8 @@ return [
          * The default configurations to be used by the opengraph generator.
          */
         'defaults' => [
-            'title'       => 'Devcentric Studio', // set false to total remove
-            'description' => 'Devcentric Studio is a leading provider of web development, web design, maintenance and support, graphic design, digital marketing, IT consulting, technology consulting, and digital transformation services. We specialize in delivering high-quality solutions tailored to meet the unique needs of our clients. With our expertise in HTML, CSS, JavaScript, PHP, and Laravel, we create custom web applications, responsive web designs, and e-commerce solutions. Our team of experienced professionals ensures that your website is visually appealing, user-friendly, and optimized for search engines. Partner with us to transform your digital presence and achieve your business goals.', // set false to total remove
+            'title'       => 'Devcentric', // set false to total remove
+            'description' => 'Devcentric is a leading provider of web development, web design, maintenance and support, graphic design, digital marketing, IT consulting, technology consulting, and digital transformation services. We specialize in delivering high-quality solutions tailored to meet the unique needs of our clients. With our expertise in HTML, CSS, JavaScript, PHP, and Laravel, we create custom web applications, responsive web designs, and e-commerce solutions. Our team of experienced professionals ensures that your website is visually appealing, user-friendly, and optimized for search engines. Partner with us to transform your digital presence and achieve your business goals.', // set false to total remove
             'url'         => false, // Set null for using Url::current(), set false to total remove
             'type'        => false,
             'site_name'   => false,
@@ -89,8 +89,8 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Devcentric Studio', // set false to total remove
-            'description' => 'Devcentric Studio is a leading provider of web development, web design, maintenance and support, graphic design, digital marketing, IT consulting, technology consulting, and digital transformation services. We specialize in delivering high-quality solutions tailored to meet the unique needs of our clients. With our expertise in HTML, CSS, JavaScript, PHP, and Laravel, we create custom web applications, responsive web designs, and e-commerce solutions. Our team of experienced professionals ensures that your website is visually appealing, user-friendly, and optimized for search engines. Partner with us to transform your digital presence and achieve your business goals.', // set false to total remove
+            'title'       => 'Devcentric', // set false to total remove
+            'description' => 'Devcentric is a leading provider of web development, web design, maintenance and support, graphic design, digital marketing, IT consulting, technology consulting, and digital transformation services. We specialize in delivering high-quality solutions tailored to meet the unique needs of our clients. With our expertise in HTML, CSS, JavaScript, PHP, and Laravel, we create custom web applications, responsive web designs, and e-commerce solutions. Our team of experienced professionals ensures that your website is visually appealing, user-friendly, and optimized for search engines. Partner with us to transform your digital presence and achieve your business goals.', // set false to total remove
             'url'         => false, // Set to null or 'full' to use Url::full(), set to 'current' to use Url::current(), set false to total remove
             'type'        => 'WebPage',
             'images'      => [],
